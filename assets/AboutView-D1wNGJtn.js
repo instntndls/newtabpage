eb801732fa725f4647375112d12e9c2f7d1c49d7
instntndls <farn7229@gmail.com>
2024-02-26 22:20:30 +0500

    fix

--- assets/AboutView-D1wNGJtn.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./index-Cw6TdDQj.js";const e={};function r(c,o){return" about "}const n=t(e,[["render",r]]);export{n as default};
---
+++ assets/AboutView-YxR99ae2.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./index-CdTXh2NZ.js";const e={};function r(c,o){return" about "}const n=t(e,[["render",r]]);export{n as default};
