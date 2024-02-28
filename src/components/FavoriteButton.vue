6f33b68add5ae7f21f7575297cc00a09a11a4c96
instntndls <farn7229@gmail.com>
2024-02-28 19:41:32 +0500

    fix

--- src/components/FavoriteButton.vue
+++ src/components/FavoriteButton.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="size-12 bg-white bg-opacity-20 rounded-2xl flex items-center justify-center hover:bg-opacity-15">
+  <div class="size-12 bg-white bg-opacity-15 rounded-2xl flex items-center justify-center hover:bg-opacity-10 hover:backdrop-blur-[4px] backdrop-blur-[8px]">
     <slot/>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,14 @@
 
 import SearchBar from "@/components/SearchBar.vue";
 import Favorites from "@/components/Favorites.vue";
+
+
 </script>
 
 <template>
-  <img class="w-screen h-screen absolute object-cover blur-2xl opacity-80 brightness-50 select-none" src="/src/assets/background.jpg" alt="Backdrop">
+  <img class="w-screen h-screen absolute object-cover blur-xl opacity-80 brightness-[0.5] select-none"
+       src="/src/assets/background.jpg"
+       alt="Backdrop">
   <div class="Frame flex flex-col w-screen h-screen items-center justify-center gap-8">
     <SearchBar />
     <Favorites />
